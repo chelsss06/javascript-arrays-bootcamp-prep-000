ec9967e99fb622f1c15baa2250907f93b87254b6
chelss093@gmail.com <chelss093@gmail.com>
2018-09-05 22:07:21 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,16 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(array(4), element) {
+  
+  [element, ...chocolateBars]
+  
+  return chocolateBars
+  
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  
+  chocolateBars.unshift(element)
+  
+  return chocolateBars
+}
